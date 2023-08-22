3a76c464985d4cbd4fadc872bf4253d9867d2c93
Ahmad Alsaidy <alsaidyxcode@gmail.com>
2023-08-22 16:37:04 +0300

    deleted:    .config/nvim/.ignore 	deleted:    .config/nvim/.stylua.toml 	new file:   .config/nvim/after/plugin/color.lua 	new file:   .config/nvim/after/plugin/comment.lua 	new file:   .config/nvim/after/plugin/lsp.lua 	new file:   .config/nvim/after/plugin/telescope.lua 	new file:   .config/nvim/after/plugin/treesitter.lua 	modified:   .config/nvim/init.lua 	new file:   .config/nvim/lua/ahmad/init.lua 	new file:   .config/nvim/lua/ahmad/packer.lua 	new file:   .config/nvim/lua/ahmad/remap.lua 	new file:   .config/nvim/plugin/packer_compiled.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,32 @@
+require("ahmad")
+
+-- line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- current line highlighting
 vim.opt.cursorline = true
+
+-- tab indenting width
 vim.opt.tabstop=4
 vim.opt.shiftwidth=4
 vim.opt.softtabstop=4
+vim.opt.expandtab=true
 
+-- ignore case sensitive
+vim.opt.ignorecase = true
+
+-- wrap line
+vim.opt.wrap = false
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
 
 vim.cmd("colorscheme gruvbox")
---
+++ .config/nvim/lua/ahmad/packer.lua
@@ -0,0 +1,47 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- telescope
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- colors
+  use { "ellisonleao/gruvbox.nvim" }
+
+  -- treesitter
+  use {
+	  'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'
+  }
+
+  -- lsp-zero
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v2.x',
+	  requires = {
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},             -- Required
+		  {'williamboman/mason.nvim'},           -- Optional
+		  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},     -- Required
+		  {'hrsh7th/cmp-nvim-lsp'}, -- Required
+		  {'L3MON4D3/LuaSnip'},     -- Required
+	  }
+  }
+
+  -- commenting
+  use {
+      'numToStr/Comment.nvim',
+      config = function()
+          require('Comment').setup()
+      end
+  }
+
+end)
