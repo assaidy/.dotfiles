c6ed0e8200fcdb728ca4be6b5e4465d8420504ba
Ahmad Alsaidy <alsaidyxcode@gmail.com>
2023-08-31 06:04:33 +0300

    modified:   lua/ahmad/remap.lua 	modified:   ../i3/config

--- .config/nvim/lua/ahmad/remap.lua
+++ .config/nvim/lua/ahmad/remap.lua
@@ -17,6 +17,13 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+-- splits
+vim.keymap.set("n", "<leader>v", ":vsplit<CR>")
+vim.keymap.set("n", "<leader>s", ":split<CR>")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<leader>wc", "<C-w>c")
 
 
