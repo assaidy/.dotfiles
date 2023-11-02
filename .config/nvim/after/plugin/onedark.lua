5ecf1790c8efb9bdc039f0736f559a086dc0deed
Ahmad Alsaidy <alsaidyxcode@gmail.com>
2023-11-02 09:39:53 +0200

    modified:   after/plugin/onedark.lua

--- .config/nvim/after/plugin/onedark.lua
+++ .config/nvim/after/plugin/onedark.lua
@@ -1,8 +1,8 @@
 -- Lua
 require('onedark').setup  {
     -- Main options --
-    style = 'warmer', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-    transparent = true,  -- Show/hide background
+    style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+    transparent = false,  -- Show/hide background
     term_colors = true, -- Change terminal color as per the selected theme style
     ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
     cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
@@ -24,7 +24,7 @@ require('onedark').setup  {
 
     -- Lualine options --
     lualine = {
-        transparent = true, -- lualine center bar transparency
+        transparent = false, -- lualine center bar transparency
     },
 
     -- Custom Highlights --
