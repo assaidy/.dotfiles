8752166916c54ee8268604ca4ff1de3a220a0c6c
Ahmad Alsaidy <alsaidyxcode@gmail.com>
2023-11-10 06:43:28 +0200

    modified:   after/plugin/onedark.lua 	modified:   ../foot/foot.ini 	modified:   ../hypr/hyprland.conf

--- .config/nvim/after/plugin/onedark.lua
+++ .config/nvim/after/plugin/onedark.lua
@@ -2,7 +2,7 @@
 require('onedark').setup  {
     -- Main options --
     style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-    transparent = false,  -- Show/hide background
+    transparent = true,  -- Show/hide background
     term_colors = true, -- Change terminal color as per the selected theme style
     ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
     cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
@@ -24,7 +24,7 @@ require('onedark').setup  {
 
     -- Lualine options --
     lualine = {
-        transparent = false, -- lualine center bar transparency
+        transparent = true, -- lualine center bar transparency
     },
 
     -- Custom Highlights --
